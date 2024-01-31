34b975bf02c916fd99fd637439770b51f0f0a3b4
Nandini Agarwal <nandiniagarwal0509@gmail.com>
2024-02-01 00:44:22 +0530

    second

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/Bird.jpg";
+import image from "../images/krishna1.jpg";
 
 const imageAltText = "Enthusiastic third year btech graduate";
 
